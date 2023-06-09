2f2384b9703080337a588d8a2fa909e471f9fdb9
dhps-lab <dhprietos11@gmail.com>
2023-06-09 06:58:35 -0500

    Hello world with express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My app');
+const express = require('express');
+const app = express();
+const port = 3010;
+
+app.get('/', (req, res) => {
+  res.send('Hello my server on express')
+});
+
+app.listen(port, ()=> {
+  console.log('listening on port', port);
+});
