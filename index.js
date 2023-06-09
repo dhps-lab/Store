d8508816dfd071a8dd67ea0b3df69a2e0800019e
dhps-lab <dhprietos11@gmail.com>
2023-06-09 09:58:20 -0500

    Working with params in paths

--- index.js
+++ index.js
@@ -18,19 +18,44 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-  res.json({
+  res.json([
+    {
     message: 'Hello, You\'re in the home directory',
     rc: 200
-  });
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id: id,
+    name:'Product 1',
+    price: 3000
+    });
 });
 
 
 app.get('/categories', (req, res) => {
   res.json([{
-    categorie:'Grocery',
+    category:'Grocery',
     description: 'This is a category general'
   }, {
-    categorie: 'home',
+    category: 'home',
+    description: 'This is a home product'
+  }]);
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { productId, categoryId } = req.params;
+
+  res.json([{
+    productId: productId,
+    category: categoryId,
+    description: 'This is a category general'
+  }, {
+    productId: productId,
+    category: categoryId,
     description: 'This is a home product'
   }]);
 });
