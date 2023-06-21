f03eb45a2c5949a3b3fe6ff62288ed16bade1150
dhps-lab <dhprietos11@gmail.com>
2023-06-20 20:59:55 -0500

    Fixing CORS error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { request } = require('express');
 const express = require('express');
 const { faker } = require('@faker-js/faker');
+const cors = require('cors');
 
 const routerApi = require('./routes/index');
 const { errorHandler, logErrors, boomErrorHandler } = require('./middlewares/error.handler');
@@ -11,6 +12,22 @@ const port = 3010;
 
 //this is a middleware were we can use a json in the body request
 app.use(express.json());
+
+
+
+// Only cors(), whatever server can connect to our API.
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)){
+      callback(null,true);
+    }else{
+      callback(new Error('Not allowed to connect to'));
+    }
+  }
+};
+app.use(cors());
 
 routerApi(app);
 
