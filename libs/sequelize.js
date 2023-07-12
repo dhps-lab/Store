b59f74c8c1e028b29c69d506ac8f0086e930cb30
dhps-lab <dhprietos11@gmail.com>
2023-07-12 18:23:02 -0500

    Validating change of db to mysql Ok

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,10 +6,11 @@ const setupModels = require('./../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPass);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
+console.log(URI);
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   logging: false,
 });
 
