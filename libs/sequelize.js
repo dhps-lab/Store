d4b84441e17ceafad8d633509c5ecd9754657dca
dhps-lab <dhprietos11@gmail.com>
2023-07-17 18:09:43 -0500

    Initial configuration to make migrations with sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,11 +6,11 @@ const setupModels = require('./../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPass);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 console.log(URI);
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: false,
 });
 
